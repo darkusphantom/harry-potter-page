6d0948614b977c12cbf69cd360cf7880401986cc
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2021-10-22 13:12:06 -0400

    Pagina completada al 100%
    ADD:
    - Modificiacion de la barra de busqueda
    - Animaciones
    - ID a la tabla

--- src/app/modules/staff/staff.component.ts
+++ src/app/modules/staff/staff.component.ts
@@ -1,26 +1,19 @@
-// Modulos de Angular
 import { Component, OnInit } from '@angular/core';
-
-// Servicios
 import { StaffService } from '../../services/staff.service';
-
-// Interfaces
 import { Staff } from '../../interfaces/staffs.interface';
 
 @Component({
   selector: 'app-staff',
-  templateUrl: './staff.component.html'
+  templateUrl: './staff.component.html',
+  styleUrls: ['./staff.component.scss']
 })
 export class StaffComponent implements OnInit {
-  // Atributos
   public filterStaffs: string = '';
   staffs: any = [];
   public page: number = 0;
 
-  // Constructor
   constructor(private staffService: StaffService) { }
 
-  // Metodos
   ngOnInit(): void {
     this.getStaff();
   }
@@ -35,7 +28,6 @@ export class StaffComponent implements OnInit {
     if (this.page > 0)
       this.page -= 5;
   }
-
 
   nextPage(){
     this.page += 5;
--- src/app/modules/student/student.component.ts
+++ src/app/modules/student/student.component.ts
@@ -1,26 +1,19 @@
-// Modulos de Angular
 import { Component, OnInit } from '@angular/core';
-
-// Servicios
 import { StudentService } from '../../services/student.service';
-
-// Interfaces
 import { Student } from '../../interfaces/students.interface';
 
 @Component({
   selector: 'app-student',
-  templateUrl: './student.component.html'
+  templateUrl: './student.component.html',
+  styleUrls: ['./student.component.scss']
 })
 export class StudentComponent implements OnInit {
-  // Atributos
   public filterStudents: string = '';
   public students: any = [];
   public page: number = 0;
 
-  // Constructor
   constructor(private studentService: StudentService) { }
 
-  // Metodos
   ngOnInit(): void {
     this.getStudent();
   }
@@ -35,7 +28,6 @@ export class StudentComponent implements OnInit {
     if (this.page > 0)
       this.page -= 5;
   }
-
 
   nextPage(){
     this.page += 5;
